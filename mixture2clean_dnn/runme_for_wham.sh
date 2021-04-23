3ad29378250c0918e198f71a64dbed481b24417e
FYJNEVERFOLLOWS <43714882+FYJNEVERFOLLOWS@users.noreply.github.com>
2021-04-23 18:05:20 +0800

    add .sh for wham

---
+++ mixture2clean_dnn/runme_for_wham.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+MINIDATA=0
+if [ $MINIDATA -eq 1 ]; then
+  WORKSPACE="/Users/fuyanjie/Desktop/Coding/PycharmProjects/SE/sednn_xu/mixture2clean_dnn/workspace"
+#  mkdir $WORKSPACE
+  TR_SPEECH_DIR="/Users/fuyanjie/Desktop/Coding/PycharmProjects/SE/sednn_xu/mixture2clean_dnn/mini_data/train_speech"
+  TR_NOISE_DIR="/Users/fuyanjie/Desktop/Coding/PycharmProjects/SE/sednn_xu/mixture2clean_dnn/mini_data/train_noise"
+  TE_SPEECH_DIR="/Users/fuyanjie/Desktop/Coding/PycharmProjects/SE/sednn_xu/mixture2clean_dnn/mini_data/test_speech"
+  TE_NOISE_DIR="/Users/fuyanjie/Desktop/Coding/PycharmProjects/SE/sednn_xu/mixture2clean_dnn/mini_data/test_noise"
+  echo "Using mini data. "
+else
+  WORKSPACE="./wham_workspace"
+  TR_SPEECH_DIR="./wham_data/train_speech"
+  TR_MIXED_DIR="./wham_data/train_mixed"
+  TE_SPEECH_DIR="./wham_data/test_speech"
+  TE_MIXED_DIR="./wham_data/test_mixed"
+  echo "Using full data. "
+fi
+
+# Create mixture csv. 
+python prepare_data_for_wham.py create_mixture_csv --workspace=$WORKSPACE --speech_dir=$TR_SPEECH_DIR --mixed_dir=$TR_MIXED_DIR --data_type=train
+python prepare_data_for_wham.py create_mixture_csv --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --mixed_dir=$TE_MIXED_DIR --data_type=test
+
+# Calculate mixture features.
+TR_SNR=0
+TE_SNR=0 
+python prepare_data_for_wham.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TR_SPEECH_DIR --mixed_dir=$TR_MIXED_DIR --data_type=train --snr=$TR_SNR
+python prepare_data_for_wham.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --mixed_dir=$TE_MIXED_DIR --data_type=test --snr=$TE_SNR
+
+# Pack features. 
+N_CONCAT=7
+N_HOP=3
+#python prepare_data_for_wham.py pack_features --workspace=$WORKSPACE --data_type=train --snr=$TR_SNR --n_concat=$N_CONCAT --n_hop=$N_HOP
+#python prepare_data_for_wham.py pack_features --workspace=$WORKSPACE --data_type=test --snr=$TE_SNR --n_concat=$N_CONCAT --n_hop=$N_HOP
+
+# Compute scaler. 
+#python prepare_data_for_wham.py compute_scaler --workspace=$WORKSPACE --data_type=train --snr=$TR_SNR
+
+# Train. 
+LEARNING_RATE=1e-4
+#CUDA_VISIBLE_DEVICES=3
+#python main_dnn.py train --workspace=$WORKSPACE --tr_snr=$TR_SNR --te_snr=$TE_SNR --lr=$LEARNING_RATE
+
+# Plot training stat. 
+#python evaluate.py plot_training_stat --workspace=$WORKSPACE --tr_snr=$TR_SNR --bgn_iter=0 --fin_iter=10001 --interval_iter=1000
+
+# Inference, enhanced wavs will be created. 
+ITERATION=10000
+#CUDA_VISIBLE_DEVICES=3
+#python main_dnn.py inference --workspace=$WORKSPACE --tr_snr=$TR_SNR --te_snr=$TE_SNR --n_concat=$N_CONCAT --iteration=$ITERATION
+
+# Calculate PESQ of all enhanced speech. 
+#python evaluate.py calculate_pesq --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --te_snr=$TE_SNR
+
+# Calculate overall stats. 
+#python evaluate.py get_stats
+echo "job end time: `date`"
